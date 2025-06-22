5ffcfd0d9dbc2bc1978ed66a6f57d186f9f78078
Phucchan <phuctqhe176647@fpt.edu.vn>
2025-06-22 17:11:51 +0700

    update : homepage

--- src/app/features/public/homepage/homepage.component.ts
+++ src/app/features/public/homepage/homepage.component.ts
@@ -3,17 +3,42 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../../../shared/components/header/header.component';
 import { FooterComponent } from '../../../shared/components/footer/footer.component';
 import { HeroSectionComponent } from '../../../shared/components/hero-section/hero-section.component';
+import { HomepageService } from '../services/homepage.service';
 
 @Component({
-  selector: 'app-homepage',
-  standalone: true,
-  imports: [
-    CommonModule,
-    HeaderComponent,
-    FooterComponent,
-    HeroSectionComponent
-  ],
-  templateUrl: './homepage.component.html',
-  
+    selector: 'app-homepage',
+    standalone: true,
+    imports: [
+        CommonModule,
+        HeaderComponent,
+        FooterComponent,
+        HeroSectionComponent,
+
+    ],
+    templateUrl: './homepage.component.html',
+
 })
-export class HomepageComponent {}
+export class HomepageComponent {
+    blogs: any[] = [];
+
+    constructor(private homepageService: HomepageService) {
+        this.getHomePageData();
+    }
+
+    getHomePageData() {
+        this.homepageService.getHomePageData().subscribe(
+            {
+                next: (response) => {
+                    this.blogs = response.data.recentBlogs || [];
+                },
+                error: (error) => {
+                    console.error('Error fetching home page data:', error);
+                }
+            }
+        );
+    }
+    getTime(dateStr: string): string {
+        const date = new Date(dateStr);
+        return date.toLocaleDateString('vi-VN');
+    }
+}
