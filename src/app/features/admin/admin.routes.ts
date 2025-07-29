dcac407b25e7ec02cb907cc30143b48eddcfa996
Wangbinhbui <quangbinhbui26038897@gmail.com>
2025-07-29 16:27:01 +0700

    tao giao dien list-customer

--- src/app/features/admin/admin.routes.ts
+++ src/app/features/admin/admin.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { LayoutComponent } from './layout/layout.component';
 import { AdminTestComponent } from './admin-test/admin-test.component';
+// 1. Import component danh sách khách hàng
+import { ListCustomerComponent } from './list-customer/list-customer.component';
 
 export const ADMIN_ROUTES: Routes = [
   {
     path: '',
-    component: LayoutComponent, // <-- Layout component must have <router-outlet>
+    component: LayoutComponent, // Layout này sẽ được áp dụng cho tất cả các trang con
     children: [
       {
         path: '',
@@ -15,6 +17,11 @@ export const ADMIN_ROUTES: Routes = [
       {
         path: 'test',
         component: AdminTestComponent,
+      },
+      // 2. Thêm route mới cho trang quản lý khách hàng
+      {
+        path: 'customers', // URL cuối cùng sẽ là: /admin/customers
+        component: ListCustomerComponent,
       },
     ],
   },
