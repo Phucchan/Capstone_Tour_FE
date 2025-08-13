2c8ff80772c8693a5d312ec1eb30fbc9e16518b9
Mynameiskiet <104210040+Mynameiskiet@users.noreply.github.com>
2025-08-13 12:37:59 +0700

    u tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    // Đường dẫn này sẽ quét tất cả các file .html và .ts trong toàn bộ thư mục src
-    // Đây là phần quan trọng nhất để sửa lỗi của bạn
-    "./src/**/*.{html,ts}",
+    // Ghi chú: Thêm đuôi file `.css` vào đây.
+    // Điều này sẽ bảo Tailwind quét cả các file CSS để tìm các class tiện ích
+    // được sử dụng với cú pháp @apply.
+    "./src/**/*.{html,ts,css}",
   ],
   theme: {
     extend: {},
